eb3a7ba950203ca313de51240f0f3f26d51168ea
Danilo <darmito10@gmail.com>
2021-07-16 15:44:09 -0500

    gitignore

---
+++ frontend/out/_next/static/Cv7OSqU3-spuT3Dn3K5Lq/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/736-79bf81ad126837c4e17b.js","static/chunks/pages/index-9fb83891431bbb220ae1.js"],"/_error":["static/chunks/pages/_error-94ed2348718d59e1af9b.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
