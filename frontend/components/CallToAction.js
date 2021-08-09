4289fc23065bfffa41fc9b19424e155e828f4757
Danilo <44913898+darmo1@users.noreply.github.com>
2021-08-09 15:38:39 -0500

    Cta/spanish english (#28)
    * add feature call version mobile
    
    * change cta button in English version

--- frontend/components/CallToAction.js
+++ frontend/components/CallToAction.js
@@ -1,176 +1,226 @@
-import Link from "next/link";
 import React from "react";
+import Link from "next/link";
+import { useRouter } from "next/router";
 import { useWindowSize } from "../hooks/useWindowSize";
-import menuCallToActions from "../utils/menu-callToAction.";
+import {menuCallToActionsSpanish , menuCallToActionsEnglish} from "../utils/menu-callToAction.";
+
 
 const CallToAction = () => {
 
-    const [isOpenCTA , setIsOpenCTA] = React.useState(false)
-    const size = useWindowSize()
+  const router = useRouter();
+  const { locale } = router;
+
+  const menuCallToActions = locale === 'es-CO' ? menuCallToActionsSpanish : menuCallToActionsEnglish;
+  const [isOpenCTA, setIsOpenCTA] = React.useState(false);
+  const [showNumberPhone, setShowNumberPhone] = React.useState(false);
+  const [copySuccess, setCopySuccess] = React.useState(false);
+
+  const size = useWindowSize();
+  const numberCompany = React.useRef(null);
+
+  const handleCallUs = (e) => {
+    setShowNumberPhone(true);
+    navigator.clipboard.writeText("3135805122");
+    setCopySuccess(true);
+    setTimeout(() => {
+      setShowNumberPhone(false);
+      setCopySuccess(false);
+    }, 3000);
+  };
+
+  const handleCta = () => {
+    setIsOpenCTA(false);
+  };
+
+  const handleCallUsMobile = () => {};
 
   return (
     <>
       <div className={isOpenCTA ? "bg-cta  active" : "bg-cta"}></div>
+
       <div className="menu-cta">
-        <div onClick={() =>  setIsOpenCTA(!isOpenCTA)}>
-         {isOpenCTA
-         ? 
-         <>
-         <img
-            className="btn btn-close"
-            src="/close-icon.svg"
-            alt="close-button"
-          />
+        <div onClick={() => setIsOpenCTA(!isOpenCTA)}>
+          {isOpenCTA ? (
+            <img
+              className="btn btn-close"
+              src="/close-icon.svg"
+              alt="close-button"
+            />
+          ) : (
+            <img className="btn btn-close" src="/cta.svg" alt="cta-button" />
+          )}
+        </div>
+        {isOpenCTA && (
           <div className="container-cta">
-          {menuCallToActions.filter( item => {
-            if(size.width > 500 && item.title !== "Llamanos"){
-              return item
-            }
-            
-            if(size.width < 500){
-              return item
-            }
-          }).map((item, index) => {
-            
-            return (
-              <Link href={item.href}  key={index}>
-              <a>
-              <div className={`${item.className} d-flex  opt-cta`}>
-                <p>{item.title}</p>
-                <div className="d-flex">
-                  <img src={item.icon} alt="icon" className='img-cta' />
-                </div>
-              </div>
-              </a>
-              </Link>
-            );
-          })}
-        </div>
-          </>
-         :
-         <img
-            className="btn btn-close"
-            src="/cta.svg"
-            alt="cta-button"
-          />
-         }
-        </div>
-        
-        
+            {menuCallToActions.map((item, index) => {
+              if (size.width > 500 && item.title === "Llamanos" || item.title === "Call Us") {
+                return (
+                  <div
+                    className={`${item.className} d-flex  opt-cta`}
+                    onClick={handleCallUs}
+                    key={index}
+                  >
+                    <div>
+                      {item.title} <br />
+                      <p
+                        ref={numberCompany}
+                        className={
+                          showNumberPhone ? "active-opacity" : "opacity-none"
+                        }
+                      >
+                        3135805122
+                      </p>
+                      {copySuccess && <span>{locale === 'es-CO' ? 'Copiado' : 'Copied' }</span>}
+                    </div>
+
+                    <div className="d-flex">
+                      <img src={item.icon} alt="icon" className="img-cta" />
+                    </div>
+                  </div>
+                );
+              }
+
+              if (size.width < 500 && item.title === "Llamanos") {
+                return (
+                  <Link href="tel:+573135805122" key={index}>
+                    <a>
+                      <div
+                        className={`${item.className} d-flex  opt-cta`}
+                        onClick={handleCallUsMobile}
+                        key={index}
+                      >
+                        <div>{item.title}</div>
+
+                        <div className="d-flex">
+                          <img src={item.icon} alt="icon" className="img-cta" />
+                        </div>
+                      </div>
+                    </a>
+                  </Link>
+                );
+              }
+
+              return (
+                <Link href={item.href} key={index}>
+                  <a>
+                    <div
+                      className={`${item.className} d-flex  opt-cta`}
+                      onClick={handleCta}
+                    >
+                      <p>{item.title}</p>
+                      <div className="d-flex">
+                        <img src={item.icon} alt="icon" className="img-cta" />
+                      </div>
+                    </div>
+                  </a>
+                </Link>
+              );
+            })}
+          </div>
+        )}
       </div>
 
       <style jsx>{`
-
-        .bg-cta{
-          position:fixed;
+        .bg-cta {
+          position: fixed;
           bottom: -500px;
-          right:-500px;
-          background:#f9f9f9;
-          z-index:10;
+          right: -500px;
+          background: #f9f9f9;
+          z-index: 10;
           width: 500px;
-          height:500px;
+          height: 500px;
           border-radius: 50%;
           box-shadow: 0 3px 4px 0 rgb(94 94 94 / 70%);
         }
 
-        .active{
-          position:fixed;
+        .active {
+          position: fixed;
           bottom: -120px;
-          right:-100px;
-          animation: moveInLeft 0.5s ease-out; 
-        }
-        
-        @keyframes moveInLeft{
-          0%{
-              opacity: 0;
-        
-            }
-          100%{
-              opacity:1;
-        
-            }
+          right: -100px;
+          animation: moveInLeft 0.5s ease-out;
         }
 
-        a{
+        @keyframes moveInLeft {
+          0% {
+            opacity: 0;
+          }
+          100% {
+            opacity: 1;
+          }
+        }
+
+        a {
           color: black;
         }
 
-
-        .menu-cta, 
-        .menu-list{
-            position: fixed;
+        .active-opacity {
+          opacity: 1;
+          position: relative;
         }
 
-        .menu-cta{
-            right: 18px;
-            bottom: 100px;
-            z-index: 100;
-            justify-content: flex-end;
-            
+        .opacity-none {
+          opacity: 0;
+          position: absolute;
         }
 
-      
-        .menu-list{
-            justify-content: space-between;
-            align-items: center;
+        span{
+          color: blue;
+        }
+        .menu-cta,
+        .menu-list {
+          position: fixed;
         }
 
-        .opt-cta{
-           box-shadow: 0 3px 12px 0 rgb(0 0 0 / 12%), 0 9px 8px 0 rgb(0 0 0 / 11%);
-           padding: 0.5rem 1rem;
+        .menu-cta {
+          right: 18px;
+          bottom: 100px;
+          z-index: 100;
+          justify-content: flex-end;
         }
 
-        .img-cta{
-          width:35px;
-          height:35px;
+        .menu-list {
+          justify-content: space-between;
+          align-items: center;
         }
 
-        .menu-list > *{
-            margin: 0 1rem;
+        .opt-cta {
+          box-shadow: 0 3px 12px 0 rgb(0 0 0 / 12%),
+            0 9px 8px 0 rgb(0 0 0 / 11%);
+          padding: 0.5rem 1rem;
         }
 
-        .call-icon{
-            bottom: 220px;
-            right: 57px;
-        }
-        .write-icon{
-            bottom: 140px;
-            right: 127px;
-        }
-       
-
-        .location-icon{
-          bottom: 50px;
-          right: 100px;    
+        .img-cta {
+          width: 35px;
+          height: 35px;
+          position: relative;
         }
 
-        .btn-close{
-            width: 80px;
-            height: 80px;
+        .menu-list > * {
+          margin: 0 1rem;
         }
 
-        @media(max-width:500px){
-          .call-icon{
-            bottom: 250px;
-            right: 57px;
+        .call-icon {
+          bottom: 250px;
+          right: 57px;
         }
-        .write-icon{
-            bottom: 170px;
-            right: 127px;
+        .write-icon {
+          bottom: 170px;
+          right: 127px;
         }
-        .ring-icon{
-            bottom: 90px;
-            right: 127px;
+        .ring-icon {
+          bottom: 90px;
+          right: 127px;
         }
-        .location-icon{
-            bottom: 10px;
-            right: 80px
-        }
+        .location-icon {
+          bottom: 10px;
+          right: 80px;
         }
 
-
-        
-        `}</style>
+        .btn-close {
+          width: 80px;
+          height: 80px;
+        }
+      `}</style>
     </>
   );
 };
--- frontend/utils/menu-callToAction..js
+++ frontend/utils/menu-callToAction..js
@@ -1,4 +1,4 @@
-const menuCallToActions = [
+export const menuCallToActionsSpanish = [
   {
     title: "Te llamamos",
     icon: "/call-icon.svg",
@@ -25,4 +25,29 @@ const menuCallToActions = [
   },
 ];
 
-export default menuCallToActions;
+export const menuCallToActionsEnglish = [
+  {
+    title: "We call you",
+    icon: "/call-icon.svg",
+    className: "menu-list call-icon",
+    href: "/#form",
+  },
+  {
+    title: "Write Us",
+    icon: "/whatsapp.svg",
+    className: "menu-list write-icon",
+    href: "https://api.whatsapp.com/send?phone=573014337318",
+  },
+  {
+    title: "Visit Us",
+    icon: "/location-button.svg",
+    className: "menu-list location-icon",
+    href: "/contactanos",
+  },
+  {
+    title: "Call Us",
+    icon: "/ring-icon.svg",
+    className: "menu-list ring-icon",
+    href: "#",
+  },
+];
