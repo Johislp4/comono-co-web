22be2db2b8fc4828bddd606da7b7298de2938770
johislp4 <johislp4@gmail.com>
2021-08-11 19:35:38 -0500

    Finishing styles for the internal Blog page

--- frontend/pages/blog/index.js
+++ frontend/pages/blog/index.js
@@ -28,9 +28,12 @@ const Blog = () => {
       .catch(console.error)
   }, [])
 
+  if (blogData.length === 0) return <div>Loading...</div>
+
   let filteredPosts = blogData.filter((post) => {
     return post.title.toLowerCase().includes(search.toLowerCase())
   })
+
 
   return (
     <div className="bg-green">
@@ -39,20 +42,26 @@ const Blog = () => {
         <div className="search-bar">
           <input
             type="text"
-            placeholder="Search..."
             value={search}
             onChange={(e) => { setsearch(e.target.value) }}
           />
+          <div>
+            <img src="/search.png" alt="search" />
+            <p>Buscar</p>
+          </div>
         </div>
         <div className='container-post'>
-          {filteredPosts && filteredPosts.map((post, index) => (
-            <Link href={'/blog/' + post.slug.current} key={index} passHref>
-              <a>
-                <CardPost post={post} />
-              </a>
-            </Link>
-
-          ))}
+          {filteredPosts && filteredPosts.length > 0 ? (
+            filteredPosts.map((post, index) => {
+              return <Link href={'/blog/' + post.slug.current} key={index} passHref>
+                <a>
+                  <CardPost post={post} />
+                </a>
+              </Link>
+            })
+          ) : (
+            <h2>No hay resultados para la búsqueda</h2>
+          )}
         </div>
 
       </div>
@@ -72,6 +81,7 @@ const Blog = () => {
       }
       
       .search-bar {
+        position:relative;
         margin-top: 1rem;
       }
       
@@ -81,11 +91,33 @@ const Blog = () => {
         background: #7dab32;
         border: 0;
       }
+
+      .search-bar > div{
+        width: fit-content;
+        display: flex;
+        align-items: center;
+        position: absolute;
+        right: 0;
+        top: 30%;
+        color: #ffffff;
+        margin-right:1rem;
+      }
+
+      .search-bar img{
+        width:1.2rem;
+        margin-right:0.5rem;
+      }
       
       h1 {
         color: #ffffff;
         padding-top: 2rem;
         font-size: 3rem;
+      }
+
+      h2{
+        text-align: center;
+        width:100%;
+        padding: 2rem 0;
       }
       
       @media (max-width: 760px) {
